fabb9e78757ba5e2b91118438d5e6d996d0a710b
roohola zamani <roohola2434@gmail.com>
2025-05-13 13:12:53 +0330

    add pages

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @SpringBootApplication
+@EnableTransactionManagement
 @EntityScan(basePackages = {
 		"com.example.demo.model"
 })
