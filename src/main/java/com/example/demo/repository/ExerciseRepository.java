a8e27b9a80380cce2a0a1deda1b8d774d15d2216
roohola zamani <roohola2434@gmail.com>
2025-06-29 21:22:46 +0330

    add dockrer file and remoeve unnessesery dependency and add new endpoint for get student analyze

--- src/main/java/com/example/demo/repository/ExerciseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Exercise;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
-    Optional<Exercise> findByLessonId(Long lessonId);
-}
